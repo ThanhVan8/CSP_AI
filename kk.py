35107f5fbc37187003cc26f9f7504beddc3c75d9
Hoang Kim <vonguyenhoangkim14022003@gmail.com>
2023-07-24 00:34:09 +0700

    lv2 is done

--- kk.py
+++ kk.py
@@ -1,30 +1,23 @@
 class CSP:
     assignment = {}
-    # negCarry = 0
-    # ONE + ONE = TWO 
     def __init__(self):
         s = open("input.txt").read()
-        # inp = [[ONE+ONE], [TWO]] 
         inp = s.split("=")
         # result = TWO
         self.result = inp[1]
 
         self.operators = []
         self.operands = []
-        operandTmp = ""   # each operand
+        operandTmp = ""   
         for i in inp[0]:
             if(i.isalpha()):
-                # operandTmp = [ONE]
                 operandTmp += i
             else:
-                # operators = [+]
                 self.operators.append(i)
-                # operands = [ONE, ONE]
                 self.operands.append(operandTmp)
                 operandTmp = ""
         self.operands.append(operandTmp)
 
-        # lấy số lượng từ dài nhất
         self.maxStep = max(max((len(op) for op in self.operands)), (len(self.result)))
         self.leadingLetters = set(x[0] for x in self.operands)  # first letter of each operands
         self.leadingLetters.add((self.result[0]))
@@ -41,19 +34,7 @@ class CSP:
             if self.operators[row - 1] == '+':
                 res += val
             elif self.operators[row - 1] == '-':
-                if(res<val):
-                    # if(abs(res)<val):
-                    #     # tăng biến nhớ lên
-                    #     res = abs(res)+10 - val
-                    # elif (abs(res)>=val): 
-                    #     res = abs(res)-val   
-                    # return -res
-                    # # self.negCarry +=-1\
-                    # nếu số bị trừ nhỏ hơn, mượn 10 - đi số trừ
-                    res = res*10 - val
-                    # đánh dấu là có mượn 10
-                    return -res
-                else: res -= val
+                res -= val
         return res
 
     def checkConstraints(self, letter, value):
@@ -62,9 +43,6 @@ class CSP:
             return False
         # gán giá trị 0 cho chữ cái đầu
         if letter in self.leadingLetters and value == 0:
-            return False
-        # gán giá trị âm cho kết quả cuối cùng
-        if letter == self.result[0] and value < 0:
             return False
         return True
 
@@ -104,7 +82,6 @@ class CSP:
                             resStep = self.compute(resStep, self.assignment.get(char), row)
                             isSuccess = self.backtrack(step, row + 1, resStep, carry)  # go to next row
                             if isSuccess:
-                                print(resStep)
                                 return True
                             else:
                                 resStep = resPrev   # back to the old result
@@ -116,23 +93,27 @@ class CSP:
             else: char = self.result[-step-1] # get the character of result
 
             # xử lý kq âm
-            # kiểm tra coi có mượn 10 khong
+            tmpCarry = 0
             if(resStep<0):
-                # biến nhớ được gán tại đây
-                carry = -1
-                resStep *=-1
-            else: carry= 0
-
+                tmp = resStep
+                standard = 0
+                while(abs(tmp)!=0):
+                    standard +=10
+                    tmp = int(tmp/10)
+                resStep = standard + resStep
+                tmpCarry = -int(standard / 10)
+                
             strRes = str(resStep)  # convert result of current column to string
             correctDigit = int(strRes[-1])  # get the last digit
             if correctDigit == resStep: # if the result has 1 digit
-                carryNext = carry
+                carryNext = tmpCarry
             else: 
                 carryNext = int(strRes[0:len(strRes)-1])
-            if(char =='0'):
-                isSuccess = self.backtrack(step + 1, 0, carryNext, carryNext) # go to next step
-                if isSuccess:
-                    return True
+            # đối với giá trị 
+            if(char =='0' and correctDigit ==0):
+                return True
+            elif (char== '0' and correctDigit !=0):
+                return False
             if self.isAssigned(char):   # if already assigned
                 if self.assignment.get(char) == correctDigit:   # if the value of assigned letter is valid
                     isSuccess = self.backtrack(step + 1, 0, carryNext, carryNext) # go to next step
