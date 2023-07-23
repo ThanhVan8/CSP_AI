5cfe7dc79e734a072885fa37777600c7c2bc7970
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-07-24 02:16:10 +0700

    lv1

--- csp_right-left.py
+++ csp_right-left.py
@@ -53,6 +53,8 @@ class CSP:
     # resStep: result of current step
     # carry
     def backtrack(self, step, row, resStep, carry):
+        # print(step,row)
+        # print(self.assignment)
         if step > self.maxStep - 1:
             return True
                 
@@ -70,6 +72,7 @@ class CSP:
                         return True
                 else:   # if not assigned yet
                     for i in range(10): # from 0-9
+                    # for i in range(9,-1,-1):
                         if self.checkConstraints(char, i):
                             self.assignment[char] = i
                             resPrev = resStep   # store the old result in case backtrack fail
@@ -82,15 +85,34 @@ class CSP:
                                 self.assignment.pop(char)   # pop out the value causing fail 
             
         else:   # handle the result (right side of '=')
-            char = self.result[-step-1] # get the character of result
-
             strRes = str(resStep)  # convert result of current column to string
-            correctDigit = int(strRes[-1])  # get the last digit
-            if correctDigit == resStep: # if the result has 1 digit
-                carryNext = 0
+            if resStep < 0:
+                tmp = 0
+                if abs(resStep) % 10 == 0:
+                    tmp = abs(resStep) / 10
+                else:
+                    tmp = len(strRes) - 1
+                correctDigit = tmp*10 + resStep
+                carryNext = -tmp
             else:
-                carryNext = int(strRes[0:len(strRes)-1])
+                correctDigit = int(strRes[-1])  # get the last digit
+                if correctDigit == resStep: # if the result has 1 digit
+                    carryNext = 0
+                else:
+                    carryNext = int(strRes[0:len(strRes)-1])
 
+            if step > len(self.result) - 1: # if go beyond the first letter of result
+                # print(correctDigit)
+                if correctDigit == 0:
+                    isSuccess = self.backtrack(step + 1, 0, carryNext, carryNext) # go to next step
+                    if isSuccess:
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+                
+            char = self.result[-step-1] # get the character of result
             if self.isAssigned(char):   # if already assigned
                 if self.assignment.get(char) == correctDigit:   # if the value of assigned letter is valid
                     isSuccess = self.backtrack(step + 1, 0, carryNext, carryNext) # go to next step
